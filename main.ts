4723afde9a554e46973846ee67963aba8598981c
hellooscar <125448089+hellooscar@users.noreply.github.com>
2023-02-17 18:55:40 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.Black))
+strip.show()
 wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S1, 90)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P1) == 1 || pins.digitalReadPin(DigitalPin.P1) == 1) {
@@ -7,6 +10,8 @@ basic.forever(function () {
         basic.pause(500)
         wuKong.setAllMotor(100, 100)
         basic.pause(1000)
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
     } else if (sonar.ping(
     DigitalPin.P12,
     DigitalPin.P13,
@@ -15,7 +20,19 @@ basic.forever(function () {
         wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S1, 90)
         basic.pause(2000)
         wuKong.setAllMotor(100, 100)
+    } else if (sonar.ping(
+    DigitalPin.P12,
+    DigitalPin.P13,
+    PingUnit.Centimeters
+    ) > 10 && sonar.ping(
+    DigitalPin.P12,
+    DigitalPin.P13,
+    PingUnit.Centimeters
+    ) <= 10) {
+        wuKong.setAllMotor(100, 100)
     } else {
         wuKong.setAllMotor(100, 0)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        strip.show()
     }
 })
